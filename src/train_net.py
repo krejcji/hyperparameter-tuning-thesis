9755fd6d1f81007cb5ab2208bb7756f4bc265122
Jiří Krejčí <rostuli27@gmail.com>
2024-04-02 12:29:40 +0200

    Added checkpoint support and modified logging

--- src/train_net.py
+++ src/train_net.py
@@ -1,52 +1,82 @@
 from tqdm import tqdm
 import torch
-import wandb
 import optuna
 from torchinfo import summary
 import numpy as np
+from pathlib import Path
+import time
+from datetime import datetime
 
 from load_model import load_model
 import dl_utils
 
-def train_net(train_loader, val_loader, params, config, trial=None):
-    n_epochs = params['epochs']
+def train_net(train_loader, val_loader, params, config, logger,
+        trial=None, previous_epoch=0, end_epoch=0, checkpoint_path=None):
     batch_size = config['data']['batch_size']
-    input_channels = config['data']['input_dim']
-    input_length = config['data']['input_length']
+    input_dim = config['data']['input_dim']
+    input_size = [batch_size] + input_dim
+    input_size = tuple(input_size)
 
-    model = load_model(config)
-    summary(model, input_size=(batch_size, input_channels, input_length))
-
-    wandb.watch(model)
+    # Load model
+    model = load_model(config, params=params)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model.to(device)
+    summary(model, input_size=input_size)
 
+    # Optimizer
+    if params['optimizer'] == 'SGD':
+        opt = torch.optim.SGD(model.parameters(), lr=params['learning_rate'])
     if params['optimizer'] == 'AdamW':
         opt = torch.optim.AdamW(model.parameters(), lr=params['learning_rate'])
+    elif params['optimizer'] == 'Adam':
+        opt = torch.optim.Adam(model.parameters(), lr=params['learning_rate'])
     else:
         raise NotImplementedError('Optimizer not supported')
-
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
         opt, mode='min', factor=0.1, patience=5)
 
+    # Loss function
     if params['loss'] == 'BCEWithLogitsLoss':
         loss_fn = torch.nn.BCEWithLogitsLoss()
     elif params['loss'] == 'MSELoss':
         loss_fn = torch.nn.MSELoss()
+    elif params['loss'] == 'CrossEntropyLoss':
+        loss_fn = torch.nn.CrossEntropyLoss()
     else:
         raise NotImplementedError('Loss function not supported')
 
+    # Out activation
     if params['out_activation'] == 'Sigmoid':
         out_activation = torch.nn.Sigmoid()
     elif params['out_activation'] == 'None':
         out_activation = torch.nn.Identity()
+    elif params['out_activation'] == 'Softmax':
+        out_activation = torch.nn.Softmax(dim=1)
     else:
         raise NotImplementedError('Activation function not supported')
 
+    # Set checkpoint, experiment id, epochs
+    if checkpoint_path is not None:
+        path = Path(checkpoint_path)
+        last_dir = path.parent.name
+        name = checkpoint_path.split('_')[-1]
+        id = f"{last_dir}_{name}"
+        if previous_epoch > 0:
+            load_checkpoint(model, opt, checkpoint_path)
+        n_epochs = end_epoch - previous_epoch
+    else:
+        n_epochs = params['epochs']
+        id = datetime.now().strftime("%Y%m%d-%H%M%S")
+        previous_epoch = 0
+        end_epoch = n_epochs
+    model.to(device)
+
+    # Logging
+    logger.init_run(model, id, params)
     train_loss = []
     val_loss = []
 
-    for epoch in range(n_epochs):
+    for epoch in range(previous_epoch, end_epoch):
+        start_time = time.time()
         # Training
         loss_per_step = []
         model.train()
@@ -55,7 +85,7 @@ def train_net(train_loader, val_loader, params, config, trial=None):
             for x,y in per_epoch:
                 opt.zero_grad(set_to_none=True)
                 per_epoch.set_description(f"Epoch: {epoch+1}/{n_epochs}")
-                x,y = x.to(device, torch.float32), y.to(device, torch.float32)
+                x,y = x.to(device), y.to(device)
                 y_hat = model(x)
                 loss = loss_fn(y_hat, y)
                 loss_per_step.append(loss.item())
@@ -65,6 +95,7 @@ def train_net(train_loader, val_loader, params, config, trial=None):
                 per_epoch.set_postfix(train_loss=loss.item())
             if scheduler.__module__ == 'torch.optim.lr_scheduler':
                 scheduler.step(1)
+
         train_loss.append(sum(loss_per_step)/len(loss_per_step))
 
         val_loss_per_step = []
@@ -77,9 +108,7 @@ def train_net(train_loader, val_loader, params, config, trial=None):
                 y_pred = []
                 for x_val,y_val in per_val_epoch:
                     per_val_epoch.set_description("Model Evaluation: ")
-                    x_val,y_val = x_val.to(device, torch.float32
-                                        ), y_val.to(device,
-                                                    torch.float32)
+                    x_val,y_val = x_val.to(device), y_val.to(device)
                     y_hat_val = model(x_val)
 
                     y_pred.append(out_activation(y_hat_val).cpu().detach().numpy())
@@ -95,19 +124,41 @@ def train_net(train_loader, val_loader, params, config, trial=None):
         losses = {'val_loss': val_loss[-1], 'train_loss': train_loss[-1]}
         metrics = dl_utils.evaluate_metrics(config, np.concatenate(y_true), np.concatenate(y_pred))
         metrics.update(losses)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
 
-        wandb.log(metrics, step=epoch)
+        print (f"loss: {train_loss[-1]:.4e}, val_loss: {val_loss[-1]:.4e} ")
 
-        # Early-stopping
-        if 'pruning' in config['hp_optimizer'] and config['hp_optimizer']['pruning']:
-            if config['hp_optimizer']['name'] == 'Optuna':
-                if trial is None:
-                    raise ValueError('Trial object not provided')
-                trial.report(val_loss[-1], epoch)
-                # Don't prune first 4 epochs because possible unstability
-                if epoch > 4 and trial.should_prune():
-                    wandb.finish()
-                    raise optuna.TrialPruned()
+        logger.log(metrics, epoch+1, elapsed_time)
 
-    wandb.finish()
-    return val_loss[-1]
+        # Early-stopping for Optuna
+        if trial is not None:
+            trial.report(val_loss[-1], epoch)
+            if trial.should_prune():
+                logger.finish()
+                raise optuna.TrialPruned()
+
+    logger.finish()
+
+    # Save checkpoint and return evaluation info
+    if checkpoint_path is not None:
+        save_checkpoint(model, opt, end_epoch, checkpoint_path)
+        evaluated_info = [
+            {'epoch': i + previous_epoch + 1, 'metric': val_loss[i], 'loss': train_loss[i]
+            } for i in range(len(val_loss))
+        ]
+        return evaluated_info
+    else:
+        return val_loss[-1]
+
+def save_checkpoint(model, optimizer, epoch, checkpoint_path):
+    torch.save({
+        'epoch': epoch,
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict()
+    }, checkpoint_path)
+
+def load_checkpoint(model, optimizer, checkpoint_path):
+    checkpoint = torch.load(checkpoint_path)
+    model.load_state_dict(checkpoint['model'])
+    optimizer.load_state_dict(checkpoint['optimizer'])

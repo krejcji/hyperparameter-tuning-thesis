0850dfc0d60091ed7d1f6c8abe7b2e02486df39d
Jiří Krejčí <rostuli27@gmail.com>
2024-04-02 12:30:30 +0200

    Removed logger from dl_utils

--- src/dl_utils.py
+++ src/dl_utils.py
@@ -1,8 +1,7 @@
 import torch
 from sklearn.metrics import roc_auc_score
+from sklearn.metrics import accuracy_score
 import numpy as np
-import os
-import pathlib
 
 def should_prune_ptbxl(train_loss, val_loss):
     return np.var(val_loss[-5:]) < 0.2e-6 or \
@@ -25,22 +24,6 @@ def evaluate_mse(net, criterion, testloader, verbose=False):
         print(f'Mean loss: {mean_loss}')
     return mean_loss
 
-def evaluate_accuracy(net, data_loader, verbose=False):
-    with torch.no_grad():
-        net.eval()
-        correct = 0
-        total = 0
-        for data in data_loader:
-            inputs, labels = data
-            outputs = net(inputs)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-    accuracy = correct / total
-    if verbose:
-        print(f'Accuracy: {accuracy:.3f}')
-    return accuracy
-
 def evaluate_metrics(config, y_true, y_pred):
     metrics = config['metrics']
     results = {}
@@ -55,36 +38,14 @@ def evaluate_metrics(config, y_true, y_pred):
             raise ValueError(f"Unknown metric: {metric}")
     return results
 
+def evaluate_accuracy(y_true, y_pred, verbose=False):
+    accuracy = accuracy_score(y_true, y_pred.argmax(axis=1))
+    if verbose:
+        print(f'Accuracy: {accuracy}')
+    return accuracy
+
 def evaluate_auc(y_true, y_pred, verbose=False):
     auc = roc_auc_score(y_true[:,:5], y_pred[:,:5], average='macro') # TODO: Fix
     if verbose:
         print(f'Macro AUC: {auc}')
     return auc
-
-class CSV_Logger():
-    def __init__(self, path, header):
-        self.header = header
-        i = 0
-
-        dir = path.parent
-        name = path.name
-        filename = name
-
-        while((dir / (filename + '.csv')).exists()):
-            filename = name + f'_{i}'
-            i = i + 1
-
-        self.filename = dir / (filename + '.csv')
-
-        print(f'Filename: {self.filename}')
-
-        with open(self.filename, 'w') as f:
-            f.write(header + '\n')
-
-    def log_error(self, *args):
-        with open(self.filename, 'a') as f:
-            for i in range(len(args)):
-                if i != 0:
-                    f.write(', ')
-                f.write(str(args[i]))
-            f.write('\n')

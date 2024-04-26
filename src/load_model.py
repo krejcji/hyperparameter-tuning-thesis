e7dee24223b48fe0d7d50990ef046e87d0afb211
Jiří Krejčí <rostuli27@gmail.com>
2024-04-26 17:26:20 +0200

    Some models were renamed.

--- src/load_model.py
+++ src/load_model.py
@@ -2,26 +2,32 @@
 Load the model to be used for training based on the parameters from configuration file.
 """
 def load_model(params):
-    if params['model']['name'] == 'xresnet1d':
-        from xResnet1d import xresnet1d
+    match params['model']:
+        case 'xresnet1d':
+            from xResnet1d import xresnet1d
 
-        model_dropout = params['model']['model_dropout']
-        original_f_number = params['model']['original_f_number']
-        fc_dropout = params['model']['fc_dropout']
+            # model_dropout = params['model_dropout']
+            # original_f_number = params['original_f_number']
+            # fc_dropout = params['fc_dropout']
 
-        model = xresnet1d.xresnet1d101(model_drop_r=model_dropout,
-                        original_f_number=original_f_number,
-                        fc_drop=fc_dropout)
-    elif params['model']['name'] == 'CNN':
-        from cnn_net.cnn import CNNNet
-        model = CNNNet(params)
-    elif params['model']['name'] == 'CNN_2D':
-        from cnn_net.cnn_2d import CNN2DNet
-        model = CNN2DNet(params)
-    elif params['model']['name'] == 'CNN_2D_simple':
-        from cnn_net.cnn_2d_simple import CNN2DSimpleNet
-        model = CNN2DSimpleNet(params)
-    else:
-        raise ValueError(f"Unknown model: {params['model']['name']}")
+            model_dropout = 0.2
+            original_f_number = True
+            fc_dropout = 0.3
+
+
+            model = xresnet1d.xresnet1d34(model_drop_r=model_dropout,
+                            original_f_number=original_f_number,
+                            fc_drop=fc_dropout)
+        case 'CNN_1D':
+            from cnn_net.cnn_1d import CNNNet
+            model = CNNNet(params)
+        case 'CNN_2D':
+            from cnn_net.cnn_2d import CNN2DNet
+            model = CNN2DNet(params)
+        case 'CNN_2D_simple':
+            from cnn_net.cnn_2d_simple import CNN2DSimpleNet
+            model = CNN2DSimpleNet(params)
+        case _:
+            raise ValueError(f"Unknown model: {params['model']}")
 
     return model

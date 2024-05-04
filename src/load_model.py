d9d381ac4c7820c89585b5e9da56d85812a11345
Jiří Krejčí <rostuli27@gmail.com>
2024-05-04 08:41:15 +0200

    Added LSTM, reparametrized xResnet

--- src/load_model.py
+++ src/load_model.py
@@ -6,18 +6,15 @@ def load_model(params):
         case 'xresnet1d':
             from xResnet1d import xresnet1d
 
-            # model_dropout = params['model_dropout']
-            # original_f_number = params['original_f_number']
-            # fc_dropout = params['fc_dropout']
-
-            model_dropout = 0.2
-            original_f_number = True
-            fc_dropout = 0.3
-
+            model_dropout = params.get('model_dropout', 0.3)
+            fc_dropout = params.get('fc_dropout', 0.3)
+            original_f_number = params.get('original_f_number', False)
+            data_dim = params['data']['input_dim'][0]
+            out_dim = params['data']['output_dim']
 
             model = xresnet1d.xresnet1d34(model_drop_r=model_dropout,
                             original_f_number=original_f_number,
-                            fc_drop=fc_dropout)
+                            fc_drop=fc_dropout, in_ch=data_dim, c_out=out_dim)
         case 'CNN_1D':
             from cnn_net.cnn_1d import CNNNet
             model = CNNNet(params)
@@ -27,6 +24,9 @@ def load_model(params):
         case 'CNN_2D_simple':
             from cnn_net.cnn_2d_simple import CNN2DSimpleNet
             model = CNN2DSimpleNet(params)
+        case 'LSTM':
+            from rnn_net.lstm import LSTM
+            model = LSTM(params)
         case _:
             raise ValueError(f"Unknown model: {params['model']}")
 

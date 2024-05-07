8293004baa12d33440627dbcbf6c97b8e304d4a5
Jiří Krejčí <rostuli27@gmail.com>
2024-05-07 15:46:56 +0200

    SVHN dataset added

--- src/load_data.py
+++ src/load_data.py
@@ -2,13 +2,16 @@ from pathlib import Path
 import torch
 import torch.utils
 import torchvision.transforms.v2 as v2
+import torchvision.transforms as transforms
 from torchvision.datasets import MNIST
+from torchvision.datasets import SVHN
 
 from datasets.multifidelity_sampler import MultifidelitySampler
 
 PTBXL_PATH = Path('data') / 'ptbxl'
 KAGGLE_PPG_PATH = Path('data') / 'kaggle_ppg_bp'
 CIFAR10_PATH = Path('data') / 'cifar_10'
+SVHN_ROOT = Path('data') / 'svhn'
 
 # v2 expects CHW format
 transformation = v2.Compose([
@@ -27,19 +30,33 @@ mnist_transform = v2.Compose([
     v2.ToImage(),
     v2.ToDtype(torch.float32, scale=True),
 ])
+
+transform_svhn = v2.Compose([
+    v2.RandomResizedCrop(size=(32, 32), scale=(0.8, 1.0), interpolation=v2.InterpolationMode.BILINEAR),  # Randomly resize and crop
+    v2.RandomHorizontalFlip(p=0.5),
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Normalize((0.4376821, 0.4437697, 0.47280442), (0.19803012, 0.20101562, 0.19703614))
+])
+
+transform_svhn_val = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Normalize((0.4376821, 0.4437697, 0.47280442), (0.19803012, 0.20101562, 0.19703614))
+])
+
 #mnist_transform = v2.Compose([
 #    v2.ToTensor()
 #])
 
-def augmentation_fn(image):
+def cifar10_fn(image):
     return transformation(torch.as_tensor(image) / 255.0)
 
-def cifar10_fn(image):
+def cifar10_test_fn(image):
     return transformation_test(torch.as_tensor(image) / 255.0)
 
 def mnist_fn(image):
     return mnist_transform(image)
-
 
 def load_data(config):
     batch_size = config['data']['batch_size']
@@ -66,11 +83,15 @@ def load_data(config):
         from functools import partial
 
         cifar10 = CIFAR10(CIFAR10_PATH)
-        train = cifar10.dataset('train', augmentation_fn)
-        dev = cifar10.dataset('dev', cifar10_fn)
+        train = cifar10.dataset('train', cifar10_fn)
+        dev = cifar10.dataset('dev', cifar10_test_fn)
     elif config['data']['name'] == 'MNIST':
         train = MNIST(root='data', train=True, download=True, transform=mnist_fn)
         dev = MNIST(root='data', train=False, download=True, transform=mnist_fn)
+    elif config['data']['name'] == 'SVHN':
+        train = SVHN(root=SVHN_ROOT, split='train', download=True, transform=transform_svhn)
+        dev = SVHN(root=SVHN_ROOT, split='test', download=True, transform=transform_svhn_val)
+        # SVHN dev set has 5099 out of 26032 samples with y==1, which is 19.58% of the dataset
     else:
         raise ValueError(f"Unknown dataset: {config['data']['name']}")
 
@@ -88,6 +109,7 @@ def load_data(config):
     return trainloader, devloader
 
 if __name__ == '__main__':
-    from datasets.cifar10 import CIFAR10
-    cifar10 = CIFAR10(Path('data') / 'cifar_10')
+    train = SVHN(root=SVHN_ROOT, split='train', download=True, transform=transforms.ToTensor())
+    dev = SVHN(root=SVHN_ROOT, split='test', download=True, transform=transforms.ToTensor())
+
     print()

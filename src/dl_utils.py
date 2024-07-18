5215655ba58f4c0d00c871f9de04f532e383fa16
Jiří Krejčí <rostuli27@gmail.com>
2024-07-18 09:17:21 +0200

    refactored, added debugging option

--- src/dl_utils.py
+++ src/dl_utils.py
@@ -1,6 +1,14 @@
+"""
+Contains several utility function for training including:
+- helper function for evaluating metrics
+    - MSE metric
+    - AUC metric
+"""
 import torch
+from torch.nn import functional as F
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import hamming_loss
 import numpy as np
 
 def should_prune_ptbxl(train_loss, val_loss):
@@ -27,16 +35,42 @@ def evaluate_mse(net, criterion, testloader, verbose=False):
 def evaluate_metrics(config, y_true, y_pred):
     metrics = config['metrics']
     results = {}
-    for metric in metrics:
-        if metric == 'accuracy':
-            results['accuracy'] = evaluate_accuracy(y_true, y_pred)
-        elif metric == 'mse':
-            results['mse'] = evaluate_mse(y_true, y_pred)
-        elif metric == 'macro_auc':
-            results['macro_auc'] = evaluate_auc(y_true, y_pred)
-        else:
-            raise ValueError(f"Unknown metric: {metric}")
+    try:
+        for metric in metrics:
+            if metric == 'accuracy':
+                results['accuracy'] = evaluate_accuracy(y_true, y_pred)
+            elif metric == 'mse':
+                results['mse'] = evaluate_mse(y_true, y_pred)
+            elif metric == 'macro_auc':
+                results['macro_auc'] = evaluate_auc(y_true, y_pred)
+            elif metric == 'hamming_dist':
+                results['hamming_dist'] = hamming_dist(y_true, y_pred)
+            elif metric == 'debug':
+                debug_metrics(y_true, y_pred)
+            else:
+                raise ValueError(f"Unknown metric: {metric}")
+    except Exception as e:
+        print(f"Error evaluating metrics: {e}")
+        for metric in metrics:
+            results[metric] = 0.0
     return results
+
+def debug_metrics(y_true, y_pred):
+    # Calculate per class accuracy
+    avg = 0
+    for i in range(y_true.shape[1]):
+        acc = accuracy_score(y_true[:,i], y_pred[:,i]>0.5)
+        avg = avg + acc
+        print(f'Class {i}: {acc}')
+    print (f'Average: {avg/y_true.shape[1]}')
+
+
+def hamming_dist(y_true, y_pred, verbose=True):
+    preds = F.sigmoid(torch.from_numpy(y_pred))
+    hl = 1-hamming_loss(y_true, preds > 0.5)
+    if verbose:
+        print(f'Hamming loss: {hl}')
+    return hl
 
 def evaluate_accuracy(y_true, y_pred, verbose=False):
     accuracy = accuracy_score(y_true, y_pred.argmax(axis=1))
@@ -45,7 +79,19 @@ def evaluate_accuracy(y_true, y_pred, verbose=False):
     return accuracy
 
 def evaluate_auc(y_true, y_pred, verbose=False):
-    auc = roc_auc_score(y_true[:,:5], y_pred[:,:5], average='macro') # TODO: Fix
+    preds = F.sigmoid(torch.from_numpy(y_pred))
+    auc = roc_auc_score(y_true, preds, average='macro')
     if verbose:
         print(f'Macro AUC: {auc}')
     return auc
+
+def get_predictions(net, dataloader):
+    net.eval()
+    y_true = []
+    y_pred = []
+    with torch.no_grad():
+        for i, data in enumerate(dataloader, 0):
+            inputs, labels = data
+            y_true.append(labels)
+            y_pred.append(net(inputs))
+    return torch.cat(y_pred, dim=0), torch.cat(y_true, dim=0)

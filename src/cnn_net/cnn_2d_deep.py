3ed5b282423dbb5941f3d2bb679b1805abd70d34
Jiří Krejčí <rostuli27@gmail.com>
2024-07-18 08:32:06 +0200

    Added densenet, resnets updated.

--- src/cnn_net/cnn_2d_deep.py
+++ src/cnn_net/cnn_2d_deep.py
@@ -58,18 +58,15 @@ class CNN2DNet(nn.Module):
         self.classes = out_ch
         hidden = config.get('fc_neurons', 64)
 
-        # n_layers is 2* ( 2*l1 + 2*l2 + 1*l3)
         self.depth = config.get('depth', 3)
         if self.depth == 1: # 10 layers
-            l1, l2, l3 = 1, 1, 1
+            l1, l2, l3, l4 = 1, 1, 2, 1
         elif self.depth == 2: # 20 layers
-            l1, l2, l3 = 2, 2, 2
+            l1, l2, l3, l4 = 2, 3, 3, 2
         elif self.depth == 3: # 30 layers
-            l1, l2, l3 = 3, 3, 3
+            l1, l2, l3, l4 = 3, 4, 5, 3
         elif self.depth == 4:
-            l1, l2, l3 = 3, 4, 5 # 6+8+5=38
-        elif self.depth == 5:
-            l1, l2, l3 = 4, 5, 6 # 8+10+6=48
+            l1, l2, l3, l4 = 3, 5, 6, 3 # 6+8+5=38
         else:
             raise ValueError('Invalid depth value')
 
@@ -77,20 +74,19 @@ class CNN2DNet(nn.Module):
         self.channel_multiplier = config.get('channel_multiplier', 1)
         base_mlt = base*self.channel_multiplier
 
-        self.initial_conv = nn.Conv2d(in_ch, base_mlt, 3, 1, padding='same')
+        self.initial_conv = nn.Conv2d(in_ch, base_mlt, 7, 2, padding=3)
 
         self.res_blocks = nn.ModuleList([
             nn.ModuleList(self._make_stage(base_mlt, base_mlt, 1, l1)),
             nn.ModuleList(self._make_stage(base_mlt, 2*base_mlt, 2, l2)),
-            nn.ModuleList(self._make_stage(2*base_mlt, 2*base_mlt, 1, l2)),
             nn.ModuleList(self._make_stage(2*base_mlt, 4*base_mlt, 2, l3)),
-            nn.ModuleList(self._make_stage(4*base_mlt, 4*base_mlt, 1, l1))
+            nn.ModuleList(self._make_stage(4*base_mlt, 8*base_mlt, 2, l4)),
         ])
 
         self.final_layers = nn.Sequential(
-            nn.AvgPool2d(kernel_size=(int(self.width/4), int(self.width/4))),
+            nn.AvgPool2d(kernel_size=(int(self.width/32), int(self.width/32))),
             nn.Flatten(),
-            nn.Linear(4*base_mlt, hidden),  # Adjust input features if needed
+            nn.Linear(8*base_mlt, hidden),  # Adjust input features if needed
             nn.ReLU(),
             nn.Linear(hidden, out_ch)
         )
@@ -100,7 +96,11 @@ class CNN2DNet(nn.Module):
             [ResidualBlock(out_ch, out_ch) for _ in range(num_blocks - 1)]
 
     def forward(self, x):
+        #x = F.pad(x, (0,1,0,1))
         x = F.relu(self.initial_conv(x))
+
+        x = F.pad(x, (0,1,0,1))
+        x = F.max_pool2d(x, 3, 2)
 
         for block in self.res_blocks:
             for res_block in block:
